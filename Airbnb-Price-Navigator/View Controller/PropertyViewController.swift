ed6db34f664519a25854db1e8da739140ef284fa
iyinraphael <iyinraphael@gmail.com>
2019-09-16 05:34:12 -0700

    created UI properites for viewController

---
+++ Airbnb-Price-Navigator/View Controller/PropertyViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PropertyViewController.swift
+//  Airbnb-Price-Navigator
+//
+//  Created by Iyin Raphael on 9/14/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class PropertyViewController: UIViewController {
+
+    //MARK: - Properties
+    let headerLabel = UILabel()
+    let stackview = UIStackView()
+    let zipcodeTextField = UITextField()
+    let propertyTypeTextField = UITextField()
+    let roomTypeTextField = UITextField()
+    let bedorooms = UITextField()
+    let bathrooms = UITextField()
+    let bedTypes =  UITextField()
+    let accommodates = UITextField()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+  
+
+}
