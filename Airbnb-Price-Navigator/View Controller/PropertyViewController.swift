d509e8c73e4557bdc025c771fdf7b3a33918fb97
iyinraphael <iyinraphael@gmail.com>
2019-09-16 10:45:15 -0700

    main stackview layout set

--- Airbnb-Price-Navigator/View Controller/PropertyViewController.swift
+++ Airbnb-Price-Navigator/View Controller/PropertyViewController.swift
@@ -11,19 +11,56 @@ import UIKit
 class PropertyViewController: UIViewController {
 
     //MARK: - Properties
-    let headerLabel = UILabel()
     let stackview = UIStackView()
-    let zipcodeTextField = UITextField()
-    let propertyTypeTextField = UITextField()
-    let roomTypeTextField = UITextField()
-    let bedorooms = UITextField()
-    let bathrooms = UITextField()
-    let bedTypes =  UITextField()
-    let accommodates = UITextField()
+    let zipcodeStackView = UIStackView()
+    let propertyTypeStackView = UIStackView()
+    let roomTypeStackView = UIStackView()
+    let bedroomsStackView =  UIStackView()
+    let bathroomsStackView = UIStackView()
+    let bedAndBathStackView = UIStackView()
+    let bedTypesStackView = UIStackView()
+    let accommodatStackView = UIStackView()
+    
+    var zipcodeTextField = UITextField()
+    var propertyTypeTextField = UITextField()
+    var roomTypeTextField = UITextField()
+    var bedorooms = UITextField()
+    var bathrooms = UITextField()
+    var bedTypes =  UITextField()
+    var accommodates = UITextField()
+    var submitButton = UIButton()
+    
+    var headerLabel = UILabel()
+    var zipcodeLabel = UILabel()
+    var propertyLabel = UILabel()
+    var roomTypeLabel = UILabel()
+    var bedroomsLabel = UILabel()
+    var bathroomsLabel = UILabel()
+    var bedTypesLabel = UILabel()
+    var accommodateLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(stackview)
+        stackview.axis = .vertical
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.spacing = 5
+        stackview.distribution = .fillEqually
+        stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        stackview.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50.0).isActive = true
+        stackview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15.0).isActive = true
+        stackview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15.0).isActive = true
+        
+        
+        stackview.addArrangedSubview(headerLabel)
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackview.addArrangedSubview(zipcodeStackView)
+        zipcodeStackView.axis = .vertical
+        zipcodeStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
        
     }
     
