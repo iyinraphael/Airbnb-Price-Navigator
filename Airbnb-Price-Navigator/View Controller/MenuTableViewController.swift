57604af71ecfd165b2c22693212d741de15cebc9
iyinraphael <iyinraphael@gmail.com>
2019-10-04 10:31:31 -0700

    cancel button aded to menuView

--- Airbnb-Price-Navigator/View Controller/MenuTableViewController.swift
+++ Airbnb-Price-Navigator/View Controller/MenuTableViewController.swift
@@ -13,22 +13,41 @@ class MenuTableViewController: UIViewController {
     
     //MARK:- PROPERTIES
     var menuView: UIView!
+    var stackView: UIStackView!
+    var homeButton: UIButton!
+    var aboutButton: UIButton!
+    var discoveriesButton: UIButton!
+    var ourTeamButton: UIButton!
+    var cancelButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         menuView = UIView()
         menuView.backgroundColor = .white
-        
         view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
         view.addSubview(menuView)
-
         menuView.translatesAutoresizingMaskIntoConstraints = false
         menuView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         menuView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
         menuView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
         menuView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -400).isActive = true
         
+        cancelButton = UIButton(type: .custom)
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(named: "cancel")
+        menuView.addSubview(cancelButton)
+        cancelButton.setImage(image, for: .normal)
+        cancelButton.target(forAction: #selector(cancel), withSender: self)
+        cancelButton.topAnchor.constraint(equalTo: menuView.topAnchor, constant: 20).isActive = true
+        cancelButton.leadingAnchor.constraint(equalTo: menuView.leadingAnchor, constant: 320).isActive = true
+        cancelButton.trailingAnchor.constraint(equalTo: menuView.trailingAnchor, constant: 10).isActive = true
+        
     }
     
+    @objc func cancel() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
+
+
